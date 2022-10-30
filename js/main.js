2691d772552a4915c77a3737148e7b0614dbad83
kobalt42z <chmouel2690@gmail.com>
2022-10-30 16:02:58 +0200

    last10/30

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import doSearch from "./api/doSearch.js";
 let btn = document.querySelector("#searchBtn")
 let userInput = document.querySelector("#searchInput")
-
+let modal = document.querySelector("#mainModal")
+const myModalAlternative = new bootstrap.Modal('#mainModal', {keyboard : true})
 btn.addEventListener("click",event=>{
     doSearch(userInput.value);
 })
@@ -11,4 +12,10 @@ userInput.addEventListener("keydown",event=>{
         doSearch(userInput.value)
        
     }
+})
+
+window.addEventListener("load",event=>{
+    // modal.ariaHidden = "false"
+    myModalAlternative.show()
+    console.log("success");
 })
